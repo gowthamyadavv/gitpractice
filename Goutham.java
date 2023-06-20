1e0c0cf7c7f609f276d2ce83053631a24b046a18
gowthamyadavv <136876451+gowthamyadavv@users.noreply.github.com>
2023-06-21 00:53:08 +0530

    Create Goutham.java

---
+++ Goutham.java
@@ -0,0 +1,10 @@
+public class Goutham
+ {
+  public static void main(String[] args)
+  {
+   for(int i=0;i<=20;i++)
+    {
+     System.out.println("Hi I am Goutham running for " + i + "time");
+    }
+  }
+ }
